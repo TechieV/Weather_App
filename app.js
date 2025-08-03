72a1d5d18cc5c5e0b3ef26dbc1c5095f068929e8
TechieV <vk949476@gmail.com>
2025-08-03 20:23:14 +0530

    added forcast api and aqi api but aqi api did not added in ui

--- app.js
+++ app.js
@@ -1,28 +1,58 @@
-const baseURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=bihar&appid=";
-const baseForcast ="https://api.openweathermap.org/data/2.5/forecast?q=delhi&appid=";
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const baseForcast =
+  "https://api.openweathermap.org/data/2.5/forecast?q=";
 const apiKey = "222567df567293a90ef7773617392883";
 const metric = "&units=metric";
+const baseAQI = "https://api.waqi.info/feed/";
+const tokenAQI = "ed8524167a44b9e188c1032f2a9eb2aeec749286";
+let inputCity = document.querySelector(".input-container input");
+const btn = document.querySelector(".input-container button");
 
-const weather = async () => {
-  let data = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=222567df567293a90ef7773617392883" +
-      metric
-  );
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  inputCity = inputCity.value;
+  console.log("input ", inputCity);
+  weather(inputCity);
+});
+
+const weather = async (city) => {
+  let URL = `${baseURL}${city}&appid=${apiKey}${metric}`;
+  let forcastURL = `${baseForcast}${city}&appid=${apiKey}${metric}`;
+  let aqiURL = `${baseAQI}${city}/?token=${tokenAQI}`;
+
+  Aqi = await fetch(aqiURL);
+  data = await fetch(URL);
+  forcastData = await fetch(forcastURL); 
+  
+  Aqi = await Aqi.json(); 
+  forcastData = await forcastData.json();
   response = await data.json();
-  console.log(response);
 
+  console.log(response);
+  console.log(forcastData);
+  console.log(Aqi)
+  if (data.status == "200" || data.status == 200) {
+    document.querySelector(".container").classList.remove("none-container");
+    document.querySelector(".search-city").classList.add("none-container");
+  } else if (data.status !== "200" || data.status !== 200) {
+    
+    document.querySelector(".container").classList.add("none-container");
+    document.querySelector(".not-found").classList.remove("none-container"); 
+    document.querySelector(".search-city").classList.add("none-container");
+    }
   const weatherData = {
     humidity: response.main.humidity,
     feelLike: response.main.feels_like,
     temp: response.main.temp,
-    maxTemp : response.main.temp_max,
-    minTemp : response.main.temp_min,
-    wind : response.wind.speed, //meter per sec
-    id : response.weather[0].id,
-    main : response.weather[0].main,
-    description : response.weather[0].description
+    maxTemp: response.main.temp_max,
+    minTemp: response.main.temp_min,
+    wind: response.wind.speed, //meter per sec
+    id: response.weather[0].id,
+    main: response.weather[0].main,
+    description: response.weather[0].description,
+    city: response.name,
+  };
 
-};
-console.log(weatherData);
+  // document.querySelector('.temp h1').innerText= Math.round(weatherData.temp)+" °C";
+  console.log(weatherData);
 };
